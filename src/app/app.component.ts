ac622584b071161782e21a3f1eb0e15b77bcc175
Georgios Kaleadis <extern.kaleadis_georgios@allianz.com>
2020-05-28 16:56:40 +0200

    chore: reproduction

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+
+@UntilDestroy()
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'until-destroyed-jest-error2';
+}
